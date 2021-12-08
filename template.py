5d132d938951f8ba9a6863ecda65d85a05e43fe2
Chandler <me@chandl.io>
2021-12-07 20:57:10 -0800

    Add Template File for Solutions

---
+++ template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Template
+# Advent of Code 2021 - Day N
+# https://adventofcode.com/2021/day/<N>
+
+def parse_input(file):
+    with open(file) as data:
+        #line = data.read()
+        #lines = data.read().splitlines()
+        pass
+
+def part_a(file):
+    print(f"Part A; {file=}")
+
+def part_b(file):
+    print(f"Part B; {file=}")
+
+if __name__ == "__main__":
+    part_a("./test_input.txt")
+    #part_a("./input.txt")
+    #part_b("./test_input.txt")
+    #part_b("./input.txt")
