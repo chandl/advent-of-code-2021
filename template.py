d52fac421dff75eda2b0c0f55bf8dbc2080c0fa0
Chandler <me@chandl.io>
2021-12-12 13:18:58 -0800

    Add type hints to template

--- template.py
+++ template.py
@@ -3,17 +3,20 @@
 # Advent of Code 2021 - Day N
 # https://adventofcode.com/2021/day/<N>
 
-def parse_input(file):
+def parse_input(file: str) -> None:
+    """
+        Parse the input file and return a data structure to use for the problem.
+    """
     with open(file) as data:
         #line = data.read()
         #lines = data.read().splitlines()
         pass
 
-def part_a(file):
+def part_a(file: str) -> None:
     print(f"\n**** Part A; {file=}")
     data = parse_input(file)
 
-def part_b(file):
+def part_b(file: str) -> None:
     print(f"\n**** Part B; {file=}")
     data = parse_input(file)
 
